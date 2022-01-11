f012636ceed357f010d3f4c76ed0eebfea1add0c
Lukas <lukas.ikvm1@gmail.com>
2022-01-11 11:51:57 +0100

    Add sum method and test to App

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,26 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
     @Test
     void test() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    @DisplayName("Sum should return three for input one and two")
+    void sumOfOneAndTwoReturnsThree() {
+        App app = new App();
+
+        var result = app.sum(1,2);
+
+        assertEquals(3, result);
+
     }
 }
